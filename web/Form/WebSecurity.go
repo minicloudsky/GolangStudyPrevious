dbdbf2d225b763dbd276f360803f0b4255055834
minicloudsky <1397991131@qq.com>
2020-12-21 00:19:24 +0800

    rename dir

---
+++ web/Form/WebSecurity.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	//fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+	//fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+	//template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
+	//t, err := template.New("foo").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
+	//err = t.ExecuteTemplate(out, "T",
+	//	"<script>alert('you have been pwned')</script>")
+}
